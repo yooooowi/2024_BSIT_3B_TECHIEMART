b30efa979a918ded49e0edbc99acca205c7d2e22
yooooowi <158009050+yooooowi@users.noreply.github.com>
2024-05-20 13:51:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    </head>
+    <body>
+        <section class="header">
+            <div class="banner">
+                <div class="navbar">
+                    <img src="img/logo.png" class="logo">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="shop.php">SHOP</a></li>
+                        <li><a href="#">ORDERS</a></li>
+                        <li class="tooltip-container"><a href="login.php"><i class="fa-solid fa-user"></i><span class="tooltip">Sign In</span></a></li>
+                    </ul>
+                </div>
+                <div class="content">
+                    <h1>DISCOVER TOP-TIER SMARTPHONES HERE AT TECHIEMART</h1>
+                    <p>See the latest releases from leading brands like Apple, Samsung, and more.</br> Enjoy secure checkout, fast shipping, and expert customer service. Your mobile upgrade awaits!</p>
+                    <div>
+                        <a href="shop.php">
+                            <button type="button"><span></span>SHOP NOW</button>
+                        </a>
+                    </div>
+                </div>
+                
+
+            </div>
+
+        </section>
+<!--------features------->
+        <section class="features">
+            <h1>Features</h1>
+
+        <div class="row">
+            <div class="features-pho">
+                <img src="img/iphonefeat.png">
+                <h3>Samsung Galaxy S24 Ultra</h3>
+                
+            </div>
+            <div class="features-pho">
+                <img src="img/apple/samsungfeat.png">
+                <h3>Apple iPhone 15 Pro</h3>
+            </div>
+            <div class="features-pho">
+                <img src="img/vivofeat.png">
+                <h3>vivo X100</h3>
+            </div>
+        </div>
+        </section>
+<!---------about----------->
+        <section class="about">
+            <div class="about">
+                <div id="about-section">
+                    <h1>About Us</h1>
+                    <p>Here in Techiemart, we are passionate about bringing the latest and greatest mobile technology to the Filipino community.<br> With a commitment to quality, affordability, and customer satisfaction, we strive to be your trusted partner in finding <br> the perfect mobile device. We aim to provide an unmatched shopping experience, delivering convenience and reliability right <br> to your doorstep. Join us on our journey to connect Filipinos with the world, one phone at a time.</p>
+                </div>
+            </div>
+        </section>
+        
+<!----------footer--------->
+        <footer>
+            <section class="footer">
+                <div class="container">
+                    <div class="footer-content">
+                        <h3>Contact Us</h3>
+                        <p>Email: techiemart@gmail.com</p>
+                        <p>Phone: +0912-345-6789</p>
+                        <p>Website: techiemart.com</p>
+                    </div>
+                    <div class="footer-content">
+                        <h3>Quick Links</h3>
+                        <ul class="list">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#about-section" class="scroll-to">About</a></li>
+                            <li><a href="shop.php">Products</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-content">
+                        <h3>Follow Us</h3>
+                        <ul class="social-icons">
+                            <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.instagram.com"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.tiktok.com"><i class="fab fa-tiktok"></i></a></li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="bottom-bar">
+                    <p>&copy; 2024 TECHIEMART. All rights reserved</p>
+                </div>
+            </section>
+        </footer>
+    </body>
+
+
+</html>
